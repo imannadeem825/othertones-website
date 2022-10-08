790b9f80727e2f6a762ecaa557392f93b9de791f
Iman Nadeem <iman.nadeem.825@gmail.com>
2022-10-08 17:55:04 -0500

    feat: updated header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import logo from './images/Othertones-Logo-White.png';
+import styled from 'styled-components';
+
+const HeaderImageContainer = styled.div`
+  display: flex; 
+  justify-content: center;
+`
+
+const HeaderImage = styled.img`
+  width: 75%;
+`
 
 class App extends React.Component {
   render() {
     return (
-      <>
-        <div className="App">
-          <header className="App-header">
-              Yay! Othertones Website!
-          </header>
-        </div>
-        <div></div>
-      </>
+      <div>
+        <header>
+          <HeaderImageContainer>
+            <HeaderImage src={logo} alt="Othertones logo white" />
+          </HeaderImageContainer>
+        </header>
+      </div>
     );
   }
 }
