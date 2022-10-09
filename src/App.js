1a8d551cb4bf1d854d23e82ff1c92a5a99905012
Iman Nadeem <iman.nadeem.825@gmail.com>
2022-10-08 19:02:49 -0500

    feat: navbar addition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Link, Router,  } from 'react-router-dom';
 import logo from './images/Othertones-Logo-White.png';
 import styled from 'styled-components';
 
@@ -11,6 +12,38 @@ const HeaderImage = styled.img`
   width: 75%;
 `
 
+const NavBarbutton = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const navbarEntries = [
+  {
+    navbarTitle: "Music",
+    navbarLink: "/"
+  },  
+  {
+    navbarTitle: "Shows",
+    navbarLink: "/"
+  },  
+  {
+    navbarTitle: "Store",
+    navbarLink: "/"
+  }, 
+  {
+    navbarTitle: "Visuals",
+    navbarLink: "/"
+  },  
+  {
+    navbarTitle: "Bio",
+    navbarLink: "/"
+  },  
+  {
+    navbarTitle: "Contact",
+    navbarLink: "/"
+  },
+]
+
 class App extends React.Component {
   render() {
     return (
@@ -19,6 +52,9 @@ class App extends React.Component {
           <HeaderImageContainer>
             <HeaderImage src={logo} alt="Othertones logo white" />
           </HeaderImageContainer>
+          <NavBarbutton>
+            { navbarEntries.map((entry) => <button key={entry.navbarTitle} to={entry.navbarLink}>{entry.navbarTitle}</button>) }
+          </NavBarbutton>
         </header>
       </div>
     );
