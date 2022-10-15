cfd1dde27410435cded1082dec33e4b59febeb12
Iman Nadeem <iman.nadeem.825@gmail.com>
2022-10-15 16:34:31 -0500

    feat: add prettier

--- src/App.js
+++ src/App.js
@@ -1,73 +1,74 @@
-import React from 'react';
-import logo from './images/Othertones-Logo-White.png';
-import styled from 'styled-components';
+import React from "react";
+import logo from "./images/Othertones-Logo-White.png";
+import styled from "styled-components";
 
 const HeaderImageContainer = styled.div`
-  display: flex; 
+  display: flex;
   justify-content: center;
-`
+`;
 
 const HeaderImage = styled.img`
   width: 75%;
-`
+`;
 
 const NavBarbutton = styled.div`
   display: flex;
   justify-content: space-evenly;
-`
+`;
 
 const SectionContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-`
+`;
 const SectionComponentContainer = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 
 const SectionComponentHeader = styled.div`
   padding-top: 2rem;
   font-size: 28px;
   margin: auto;
-`
+`;
 
 const SectionComponentContent = styled.div`
   padding-top: 1rem;
-`
+`;
 
 const sectionEntries = [
   {
     navbarTitle: "Music",
     navbarLink: "/",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },  
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
   {
     navbarTitle: "Shows",
     navbarLink: "/",
-    content: "content"
-  },  
+    content: "content",
+  },
   {
     navbarTitle: "Store",
     navbarLink: "/",
-    content: "content"
-  }, 
+    content: "content",
+  },
   {
     navbarTitle: "Visuals",
     navbarLink: "/",
-    content: "content"
-  },  
+    content: "content",
+  },
   {
     navbarTitle: "Bio",
     navbarLink: "/",
-    content: "content"
-  },  
+    content: "content",
+  },
   {
     navbarTitle: "Contact",
     navbarLink: "/",
-    content: "content"
+    content: "content",
   },
-]
+];
 
 class App extends React.Component {
   render() {
@@ -78,17 +79,23 @@ class App extends React.Component {
             <HeaderImage src={logo} alt="Othertones logo white" />
           </HeaderImageContainer>
           <NavBarbutton>
-            { sectionEntries.map((entry) => <button key={entry.navbarTitle} to={entry.navbarLink}>{entry.navbarTitle}</button>) }
+            {sectionEntries.map((entry) => (
+              <button key={entry.navbarTitle} to={entry.navbarLink}>
+                {entry.navbarTitle}
+              </button>
+            ))}
           </NavBarbutton>
         </header>
-          <SectionContainer>
-            { sectionEntries.map((entry) => (
-              <SectionComponentContainer key={`section-${entry.navbarTitle}`}>
-                <SectionComponentHeader>{entry.navbarTitle}</SectionComponentHeader>
-                <SectionComponentContent>{entry.content}</SectionComponentContent>
-              </SectionComponentContainer>
-            ))}
-          </SectionContainer>
+        <SectionContainer>
+          {sectionEntries.map((entry) => (
+            <SectionComponentContainer key={`section-${entry.navbarTitle}`}>
+              <SectionComponentHeader>
+                {entry.navbarTitle}
+              </SectionComponentHeader>
+              <SectionComponentContent>{entry.content}</SectionComponentContent>
+            </SectionComponentContainer>
+          ))}
+        </SectionContainer>
       </div>
     );
   }
