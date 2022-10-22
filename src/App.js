d8eb9ec134ebd8a8f5fd152969bcbc68a9c85fc7
Iman Nadeem <iman.nadeem.825@gmail.com>
2022-10-22 17:52:31 -0500

    feat: added social media menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./images/Othertones-Logo-White.png";
 import styled from "styled-components";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { FaTiktok } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { FaYoutube } from "react-icons/fa";
 
 const HeaderImageContainer = styled.div`
   display: flex;
@@ -44,6 +51,25 @@ const NavbarLinkButton = styled.button`
   font-size: 20px;
 `;
 
+const SocialMediaMenu = styled.div`
+  position: absolute;
+  right: 0;
+  font-size: 40px;
+  padding-top: 0.5rem;
+  z-index: 1;
+`;
+
+const SocialMediaDrawer = styled.div`
+  height: 100%;
+  background-color: rgba(106, 4, 15, 0.7);
+  position: fixed;
+  right: 0;
+  padding-top: 3.5rem;
+  z-index: 1;
+  width: ${(props) => (props.isOpen ? "40px" : "0px")};
+  transition: 0.5s;
+`;
+
 const sectionEntries = [
   {
     navbarTitle: "Music",
@@ -78,11 +104,64 @@ const sectionEntries = [
   },
 ];
 
-class App extends React.Component {
-  render() {
-    return (
+const socialMediaEntries = [
+  {
+    name: "TikTok",
+    icon: <FaTiktok />,
+    navbarLink: "/",
+  },
+  {
+    name: "Twitter",
+    icon: <BsTwitter />,
+    navbarLink: "https://twitter.com/othertonesmusic/",
+  },
+  {
+    name: "Facebook",
+    icon: <BsFacebook />,
+    navbarLink: "/", //TODO: Facebook group/page needs to be set up still!
+  },
+  {
+    name: "Instagram",
+    icon: <BsInstagram />,
+    navbarLink: "https://www.instagram.com/othertonesmusic/",
+  },
+  {
+    name: "Youtube",
+    icon: <FaYoutube />,
+    navbarLink: "https://www.youtube.com/channel/UCwM0hX86m1dwZYjcPRJ35EA/", //TODO: personalize/customize link, meet eligibility requirements https://www.searchenginejournal.com/happy-youtube-url-now-can-change/121086/
+  },
+];
+
+function App() {
+  const [isSocialMediaDrawerOpen, setIsSocialMediaDrawerOpen] = useState(false);
+
+  return (
+    <div>
+      <SocialMediaDrawer isOpen={isSocialMediaDrawerOpen}>
+        {socialMediaEntries.map((entry) => (
+          <a
+            key={entry.name}
+            style={{
+              color: "white",
+              fontSize: "25px",
+              display: "block",
+              padding: "0 .5rem 1rem .5rem",
+            }}
+            href={entry.navbarLink}
+          >
+            {entry.icon}
+          </a> //TODO: don't do links, do the correct nav components
+        ))}
+      </SocialMediaDrawer>
       <div>
         <header>
+          <SocialMediaMenu>
+            <AiOutlineMenu
+              onClick={() =>
+                setIsSocialMediaDrawerOpen(!isSocialMediaDrawerOpen)
+              }
+            />
+          </SocialMediaMenu>
           <HeaderImageContainer>
             <HeaderImage src={logo} alt="Othertones logo white" />
           </HeaderImageContainer>
@@ -105,8 +184,8 @@ class App extends React.Component {
           ))}
         </SectionContainer>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
